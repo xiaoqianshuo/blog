f0cc79c94a9821738704e53dc940a4dea9224104
xiaoqianshuo <xiaoqianshuo@163.com>
2026-03-06 20:40:07 +0800

    🐎 ci: 打包配置

--- apps/web/ecosystem.config.js
+++ apps/web/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "blog-web",
-      script: "node_modules/next/dist/bin/next",
+      script: "../../node_modules/next/dist/bin/next",
       args: "start -H 0.0.0.0 -p 3000",
       cwd: __dirname,
       env: {
