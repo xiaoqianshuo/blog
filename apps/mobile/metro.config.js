b56151fd693a58712cf56fd73ea216b735e2f98b
xiaoqianshuo <xiaoqianshuo@163.com>
2026-03-06 20:58:19 +0800

    🐞 fix: 解决报错问题
    Exclude web app's node_modules to avoid file map conflicts in monorepo

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -3,4 +3,9 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname)
 
+// Exclude web app's node_modules to avoid file map conflicts in monorepo
+config.resolver.blockList = [
+  /apps\/web\/node_modules\/.*/,
+]
+
 module.exports = withNativeWind(config, { input: './assets/css/global.css' })
