3f0d43f881babf68b190738cb06fa081b0c1fafb
xiaoqianshuo <xiaoqianshuo@163.com>
2026-03-06 18:41:39 +0800

    🐞 fix(web): 打包类型报错

--- apps/web/app/api/user/[id]/route.ts
+++ apps/web/app/api/user/[id]/route.ts
@@ -1,7 +1,6 @@
-import { UserId } from "@xiaoqianshuo/types";
 import { users } from "../constant";
 
-export async function GET(_req: Request, { params }: { params: Promise<{ id: UserId }> }) {
+export async function GET(_req: Request, { params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   const user = users.find((u) => u.id === id);
   if (!user) {
